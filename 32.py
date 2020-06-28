26a540901c647fb56240c64c706290f17df82496
krishna_rijal <hope143701@gmail.com>
2020-06-28 23:58:32 +0545

    [assignment submition]

--- 32.py
+++ 32.py
@@ -1,6 +1,7 @@
-n=int(input("n="))
-dict1 = dict()
-for x in range(1,n+1):
-    d[x] = x*x
-print(dict1)
+n=int(input("Input a number "))
+d = dict()
 
+for x in range(1,n+1):
+    d[x]=x*x
+
+print(d)
--- 33.py
+++ 33.py
@@ -1,5 +1,7 @@
-n=input("n=")
-dict1=dict()
+n=int(input("Input a number "))
+d = dict()
+
 for x in range(1,n+1):
-    d[x] = x*x
-print(dict1)
+    d[x]=x*x
+
+print(d)
